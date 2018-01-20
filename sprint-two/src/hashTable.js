b3c0b1ff878313700cdd96a725767145ec92666b
BrandonALittle <brandon.a.little@gmail.com>
2018-01-20 11:35:24 -0800

    passing bare minimum tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,4 +1,4 @@
-
+//
 
 var HashTable = function() {
   this._limit = 8;
@@ -6,15 +6,49 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  let index = getIndexBelowMaxForKey(k, this._limit);
+  let insertTuple = [k, v]; // create tuple
+  let storage = this._storage;
+  let bucketArray = storage.get(index);
+  if (bucketArray === undefined) {
+    storage.set(index, [insertTuple]);
+  } else {
+    let isFound = false;
+    for (let i = 0; i < bucketArray.length; i++) {
+      if (bucketArray[i][0] === k) {
+        isFound = true;
+        bucketArray[i][1] = v;
+      }
+    }
+    if (isFound === false) {
+      bucketArray.push(insertTuple);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  let index = getIndexBelowMaxForKey(k, this._limit);
+  let storage = this._storage;
+  let arrayOfTuples = storage.get(index);
+  for (let i = 0; i < arrayOfTuples.length; i++) {
+    let currentTuple = arrayOfTuples[i];
+    if (currentTuple[0] === k) {
+      return currentTuple[1];
+    }
+  }
+
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  let index = getIndexBelowMaxForKey(k, this._limit);
+  let storage = this._storage;
+  let arrayOfTuples = storage.get(index);
+  for (let i = 0; i < arrayOfTuples.length; i++) {
+    let currentTuple = arrayOfTuples[i];
+    if (currentTuple[0] === k) {
+      arrayOfTuples.splice(i, 1);
+    }
+  }
 };
 
 
