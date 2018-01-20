530514177d20976a8b87cbd7fe21b66c995cb11c
BrandonALittle <brandon.a.little@gmail.com>
2018-01-20 11:34:59 -0800

    passing bare minimum tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // check if unique
+  let storage = this._storage;
+  if (!storage.includes(item)) {
+    storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
+  //returns bools
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage.includes(item)) {
+    let index = this._storage.indexOf(item);
+    let removed = this._storage.splice(index, 1);
+    this._storage.splice(index, 1);
+    return removed;
+  }
+  //return deleted value
 };
 
 /*
